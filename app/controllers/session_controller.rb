923869655e9774b63a23e2492455122bd82bcef5
Inam ur Rehman <inambaig776@gmail.com>
2022-08-23 18:04:57 +0500

    Added error message display

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,20 +1,23 @@
 class SessionController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    user = User.find_by(username: params[:session][:username])
-    p user
-    if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
+      flash[:notice] = "Log in successful"
+      session[:user_id] = @user.id
       redirect_to root_path
     else
+      flash[:error] = "Incorrect credentials"
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     session[:user_id] = nil
+    flash[:success] = "You have successfully logged out."
     redirect_to login_path
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,12 @@ import "controllers"
 $('.ui.dropdown')
   .dropdown()
 ;
+
+$('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
